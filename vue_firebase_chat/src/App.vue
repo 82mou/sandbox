0b64dccbc66ad63c3ddd0039d898adfb59643332
蓮子 和也 <hatsushi_kazuya@liginc.co.jp>
2018-01-11 02:31:10 +0900

    とりあえずエラーは外せた

--- vue_firebase_chat/src/App.vue
+++ vue_firebase_chat/src/App.vue
@@ -1,23 +1,81 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- ボタン類 -->
+    <div>
+      <button type="button" class="btn btn-default" @click="login">
+        匿名ユーザーでログイン
+      </button>
+      <button type="button" class="btn btn-default" @click="pushData">
+        送信
+      </button>
+    </div>
+
+    <!-- リスト -->
+    <div>
+      <ul>
+        <li v-for="item in list">{{item.name}} / {{item.message}}</li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  export default {
+    name: 'app',
+    data () {
+      return {
+        list: []  // 最新状態はここにコピーされる
+      }
+    },
+    created () {
+      // 認証チェック
+      firebase.auth().onAuthStateChanged( user => { // eslint-disable-line
+        if (user) {
+          console.log('ログイン状態.')
+          this.listen()
+        } else {
+          console.log('ログインしていない状態')
+        }
+      })
+    },
+    methods: {
+      // ログイン関数
+      login () {
+        firebase.auth().signInAnonymously().then(e => { // eslint-disable-line
+          console.log(e)
+          this.listen()
+        }).catch((error) => {
+          // ログインのエラーメッセージ
+          var errorCode = error.code; // eslint-disable-line
+          var errorMessage = error.message; // eslint-disable-line
+          console.log('ログインエラーメッセージ', errorCode, errorMessage)
+        }); // eslint-disable-line
+      },
+      // データベースの変更を購読、最新状態をlistにコピーする
+      listen () {
+        firebase.database().ref('myBoard/').on('value', snapshot => { // eslint-disable-line
+          if (snapshot) {
+            const rootList = snapshot.val()
+            let list = []
+            Object.keys(rootList).forEach((val, key) => {
+              rootList[val].id = val
+              list.push(rootList[val])
+            })
+            this.list = list
+          }
+        })
+      },
+      // ダミーデータをfirebaseに送信
+      pushData () {
+        firebase.database().ref('myBoard/').push({ // eslint-disable-line
+          name: 'test',
+          message: 'foo'
+        })
+      }
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
