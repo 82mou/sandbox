d362fc501254d22b1c9a1edfd525a74b46f3440f
蓮子 和也 <hatsushi_kazuya@liginc.co.jp>
2018-01-11 18:52:20 +0900

    一旦さいごまでやった

--- vue_firebase_chat/src/App.vue
+++ vue_firebase_chat/src/App.vue
@@ -1,11 +1,19 @@
 <template>
   <div id="app">
+    <!-- 名前の入力欄 -->
+    <label for="nameInput">名前</label>
+    <input type="txt" id="nameInput" v-model="name">
+
+    <!-- メッセージの入力欄 -->
+    <label for="nameInput">メッセージ</label>
+    <input type="txt" id="nameInput" v-model="message">
+
     <!-- ボタン類 -->
     <div>
       <button type="button" class="btn btn-default" @click="login">
         匿名ユーザーでログイン
       </button>
-      <button type="button" class="btn btn-default" @click="pushData">
+      <button type="button" class="btn btn-default" @click="sendMessage">
         送信
       </button>
     </div>
@@ -24,7 +32,9 @@
     name: 'app',
     data () {
       return {
-        list: []  // 最新状態はここにコピーされる
+        list: [],     // 最新状態はここにコピーされる
+        name: '',     // 名前
+        message: '',  // 送信メッセージ
       }
     },
     created () {
@@ -42,8 +52,8 @@
       // ログイン関数
       login () {
         firebase.auth().signInAnonymously().then(e => { // eslint-disable-line
-          console.log(e)
-          this.listen()
+          console.log(e);
+          this.listen();
         }).catch((error) => {
           // ログインのエラーメッセージ
           var errorCode = error.code; // eslint-disable-line
@@ -55,13 +65,13 @@
       listen () {
         firebase.database().ref('myBoard/').on('value', snapshot => { // eslint-disable-line
           if (snapshot) {
-            const rootList = snapshot.val()
-            let list = []
+            const rootList = snapshot.val();
+            let list = [];
             Object.keys(rootList).forEach((val, key) => {
-              rootList[val].id = val
-              list.push(rootList[val])
+              rootList[val].id = val;
+              list.push(rootList[val]);
             })
-            this.list = list
+            this.list = list;
           }
         })
       },
@@ -71,6 +81,18 @@
           name: 'test',
           message: 'foo'
         })
+      },
+      sendMessage () {
+        // 空欄の場合は実行しない
+        if (!this.name || !this.message) return
+        // 送信
+        firebase.database().ref('myBoard/').push({
+          name: this.name,
+          message: this.message
+        })
+        // 送信後inputを空にする
+        this.name = ''
+        this.message = ''
       }
     }
   }
